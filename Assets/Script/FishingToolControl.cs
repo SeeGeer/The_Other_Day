04625602c81fe3d7a533368976f3c86d850e91ff
xuefanz <xuefanz@andrew.cmu.edu>
2017-10-21 20:02:58 -0400

    reel control test

---
+++ Assets/Script/FishingToolControl.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishingToolControl : MonoBehaviour {
+    public GameObject fishingReel;
+    private bool isRotate = false;
+
+    void OnTriggerEnter(Collider other)
+    {
+        isRotate = true;
+        Debug.Log("collide");
+    }
+
+
+    void OnTriggerStay(Collider other)
+    {
+        isRotate = true;
+        Debug.Log("collide");
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        isRotate = false;
+    }
+
+    private void Update()
+    {
+        if (isRotate)
+        {
+            fishingReel.transform.Rotate(new Vector3(1f, 0f, 0f));
+        }
+        // fishingReel.transform.Rotate(new Vector3(1f, 0f, 0f));
+
+    }
+}
